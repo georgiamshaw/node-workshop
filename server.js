45b47c9b9daaad9becf47a826463eba93e406e64
tonylomax <antl.lomax@gmail.com>
2019-07-22 15:07:18 +0100

    Set-Request-Response

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 
 var message = "I am so happy to be part of the Node Girls workshop";
 
 function handler(request, response) {
-  response.writeHead(200, {"Content-type": "text/html"});
-  response.write(message);
-  response.end();
+  let endpoint = request.url;
+
+  if (endpoint == "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+
+    fs.readFile(__dirname + "/public/index.html", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  }
 }
 
 const server = http.createServer(handler);
